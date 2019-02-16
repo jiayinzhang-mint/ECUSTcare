92529127a83078f57173b1a7f22e582481626ee7
MINT <mint@inslens.com>
2019-02-16 19:05:20 +0800

    wtf

--- vue.config.js
+++ vue.config.js
@@ -4,27 +4,28 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   assetsDir: "./static",
-  configureWebpack: config => {
-    let plugins = [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            warnings: false,
-            drop_debugger: true,
-            drop_console: true
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true, //console
+              drop_debugger: false,
+              pure_funcs: ["console.log"] //移除console
+            }
           }
-        },
-        sourceMap: false,
-        parallel: true
-      }),
-      new CompressionWebpackPlugin({
-        asset: "[path].gz[query]",
-        algorithm: "gzip",
-        test: new RegExp("\\.(" + ["js", "css"].join("|") + ")$"),
-        threshold: 10240,
-        minRatio: 0.8
-      })
-    ];
+        }),
+        new CompressionWebpackPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + ["js", "css"].join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      ]
+    }
   }
 };
 
